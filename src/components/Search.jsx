556fcc2250abd7fb7b9475ea2ec67a6d89ea8d1a
AleksandrPitkin <aleksandr.pitkin@e.braude.ac.il>
2024-03-30 16:54:43 +0300

    Pop Updated Mobile

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -518,26 +518,18 @@ function TrainSchedule({
     <div className="train-schedule">
       {/* Display From, To, and Date at the top */}
       <div class="text-center mb-8">
-        <div class="flex items-center justify-center gap-4 text-xl md:text-3xl lg:text-4xl font-bold text-white">
-          <span>{source}</span>
-          <svg
-            class="h-8 w-8 lg:h-10 lg:w-10 text-accent"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          <span>{destination}</span>
-        </div>
-        <div class="mt-4 font-bold text-2xl md:text-3xl lg:text-4xl text-white">
-          {date}
-        </div>
-      </div>
+  <div class="flex items-center justify-center gap-4 text-xl md:text-3xl lg:text-4xl font-bold text-white">
+    <span>{source}</span>
+    <svg class="h-8 w-8 lg:h-10 lg:w-10 text-accent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+      <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+    </svg>
+    <span>{destination}</span>
+  </div>
+  <div class="mt-4 font-bold text-2xl md:text-3xl lg:text-4xl text-white">
+    {date}
+  </div>
+</div>
+
 
       {/* Action buttons for Back and New Search */}
       <div className="action-buttons text-center my-4">
